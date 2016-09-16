1a8d553c5cad622df2e184d1072d121ada32729e
Anastasia Maksimovskih <anastasiyamaksimovskih@gmail.com>
2016-09-16 20:32:35 +0300

    Everything works. We need to try more metrics.

--- hw1/data_vizualization.py
+++ hw1/data_vizualization.py
@@ -4,8 +4,9 @@ import random
 import matplotlib.pyplot as plt
 
 
-#returns array of [a, b] for k-fold kross-validation"
-#
+# returns array of [a, b] for k-fold kross-validation"
+# a - array of points [x, y, c] for learning
+# b - array of points [x, y, c] for testing
 def getKrossValidation(k):
     a = []    
     fin = open("chips.txt", "r") 
@@ -23,7 +24,7 @@ def getKrossValidation(k):
     for i in range(k):
         b = a[:n]
         a = a[n:]
-        res.append(a, b[::]])
+        res.append([a[::], b[::]])
         a += b    
     return res
 
@@ -32,25 +33,58 @@ def getDistance(a, b):
     d = (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
     return d
 
+
+# returns array of [x, y, c, res]
+# x, y - 
+# c - real category
+# res - counted by k nearest neighbors category
 def doKNN(dataSet, k):
-    
+    k = min(k, len(dataSet[0]))
+    res = [] 
+    for p in dataSet[1]:
+        a = [[getDistance(p[:2], x[:2]), x] for x in dataSet[0]]        
+        a.sort()
+        #print(a)
+        a = [t[1] for t in a]                    
+        s = 0
+        for i in range(k):
+            s += a[i][2]
+        if (s > k // 2):
+            s = 1
+        else:
+            s = 0
+        res.append([p[0], p[1], p[2], s])
     return res
+
+################################################################################
     
-    
-data = getKrossValidation(6)
-for i, dataSet in enumerate(data):          
+err = 0   
+
+kvNumber = 6
+neighborsNumber = 3
+
+data = getKrossValidation(kvNumber)
+for i, dataSet in enumerate(data):     
+#    if i > 2:
+#        plt.figure(2)
+# creates new window with graphics
+
     plt.subplot(2, 3, i + 1)
+    plt.axis([-1, 1.5, -1, 1.5])
+    
+    #dataSet = [points for learning, points for testing]
+    #point = [x, y, c]
     
     #points with category 0
     a = [t[:2] for t in dataSet[0] if t[2] == 0]
-    plt.plot(*zip(*a), marker='o', color='r', ls='')     
+    plt.plot(*zip(*a), marker='.', color='r', ls='')     
     
     #points with category 1    
     b = [t[:2] for t in dataSet[0] if t[2] == 1]
-    plt.plot(*zip(*b), marker='o', color='b', ls='')
+    plt.plot(*zip(*b), marker='.', color='b', ls='')
   
-    #points for testing
-    testSet = doKNN(dataSet, 1)
+    testSet = doKNN(dataSet, neighborsNumber)
+    print(testSet)
 
     c = [t[:2] for t in testSet if t[2] == t[3] == 0]
     plt.plot(*zip(*c), marker='o', color='r', ls='')     
@@ -58,12 +92,18 @@ for i, dataSet in enumerate(data):
     d = [t[:2] for t in testSet if t[2] == t[3] == 1]    
     plt.plot(*zip(*d), marker='o', color='b', ls='')     
     
-    e = [t[:2] for t in testSet if t[2] != t[3]]    
-    plt.plot(*zip(*d), marker='*', color='y', ls='')      
+    e = [t[:2] for t in testSet if t[2] != t[3] == 0]    
+    plt.plot(*zip(*e), marker='s', color='r', ls='')
     
-    plt.title(len(e), "points classified wrong")    
+    f = [t[:2] for t in testSet if t[2] != t[3] == 1]    
+    plt.plot(*zip(*f), marker='s', color='b', ls='')      
     
-    '''we need to calculate an average error for 6-fold kross-validation here'''
+    plt.title(str(len(e) + len(f)) + " points of " + str(len(testSet)) + " was classified wrong") 
     
+    err += (len(e) + len(f)) / len(testSet)
+    
+s = "number of neigbors = " + str(neighborsNumber) + ", space = R^2, Euclidean distance\n"
+s += "average error for this 6-fold kross-validation is " + str(err / kvNumber)
+plt.figure(1).suptitle(s, fontsize=14, fontweight='bold')
 
 plt.show()
